0c7ee33e49bcd0fdb1165b64905585ef983d133a
Giovanna Di Meglio <reactivegio@gmail.com>
2021-01-20 18:29:58 +0100

    [chore]: gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,21 @@ import './App.css';
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact path="./">
+        <Route exact path="/">
           <Chooser />
         </Route>
-        <Route exact path="./drilling">
+        <Route exact path="/drilling">
           <DrillingTodo />
         </Route>
-        <Route exact path="./context">
+        <Route exact path="/context">
           <ContextTodo />
         </Route>
-        <Route exact path="./redux">
+        <Route exact path="/redux">
           <ReduxTodo />
         </Route>
-        <Route exact path="./recoil">
+        <Route exact path="/recoil">
           <RecoilTodo />
         </Route>
         <Route path="*">
